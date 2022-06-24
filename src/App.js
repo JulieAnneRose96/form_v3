3e856d9c6c1aa9f4e94fc83fa14d03c8e2ddc146
JulieAnneRose96 <putri.juliana.roslan@gmail.com>
2022-06-24 15:25:30 +0800

    updated form

--- src/App.js
+++ src/App.js
@@ -3,66 +3,73 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>Bootstrap Form Version-3</h1>
-      <form class = "form-horizontal" role = "form">
+      <form className = "form-horizontal" role = "form">
    
-          <div class = "form-group">
-              <label for = "firstname" class = "col-sm-2 control-label">First Name</label>
+          <div className = "form-group">
+              <label for = "firstname" className = "col-sm-2 control-label">First Name</label>
             
-              <div class = "col-sm-10">
-                <input type = "text" class = "form-control" id = "firstname" placeholder = "Enter First Name" />
+              <div className = "col-sm-6">
+                <input type = "text" className = "form-control" id = "firstname" placeholder = "Enter First Name" />
               </div>
           </div>
           
-          <div class = "form-group">
-              <label for = "lastname" class = "col-sm-2 control-label">Last Name</label>
+          <div className = "form-group">
+              <label for = "lastname" className = "col-sm-2 control-label">Last Name</label>
             
-              <div class = "col-sm-10">
-                <input type = "text" class = "form-control" id = "lastname" placeholder = "Enter Last Name" />
+              <div className = "col-sm-6">
+                <input type = "text" className = "form-control" id = "lastname" placeholder = "Enter Last Name" />
               </div>
           </div>
 
-          <label for = "name">Gender </label>
-
-          <div class = "radio">
-            <label>
-                <input type = "radio" name = "optionsRadios" id = "optionsRadios1" value = "option1" checked /> Female
-                
-            </label>
-          </div>
-
-
-          <div class = "radio">
-            <label>
-                <input type = "radio" name = "optionsRadios" id = "optionsRadios2" value = "option2" /> 
-                Male
-            </label>
-          </div>
-
-          <label for = "name">Nationality</label>
-
-          <div class = "form-group">
-            <select class = "form-control">
-              <option>Malaysian</option>
-              <option>Non-Malaysian</option>
-            </select>
-
+          <div className = "form-group">
+              <label for = "lastname" className = "col-sm-2 control-label">Age</label>
             
+              <div className = "col-sm-2">
+                <input type = "text" className = "form-control" id = "age" placeholder = "Enter your Age" />
+              </div>
+          </div>
+
+          <div className='form-group'>
+            <label for = "name" className = "col-sm-2 control-label">Gender </label>
+
+            <div className = "col-sm-2 radio">
+              <label>
+                  <input type = "radio" name = "optionsRadios" id = "optionsRadios1" value = "female" checked /> Female    
+              </label>
+            </div>
+
+            <div className = "col-sm-2 radio">
+            <label>
+                <input type = "radio" name = "optionsRadios" id = "optionsRadios2" value = "male" /> Male
+            </label>
+          </div>
+          </div>
+
+          <div className = "form-group">
+            <label for = "name" className = "col-sm-2 control-label" >Nationality</label>
+              <div className = " col-sm-6 form-group">
+                <select className = "col-sm-6  form-control">
+                  <option>Malaysian</option>
+                  <option>Non-Malaysian</option>
+                </select>
+              </div>
+          </div>
+
+
+          <div class="form-group">
+            <div class="col-sm-4 form-check">
+              <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required />
+              <label class="form-check-label" for="invalidCheck">
+                Agree to terms and conditions
+              </label>
+              <div class="invalid-feedback">
+                You must agree before submitting.
+              </div>
+            </div>
         </div>
-          
-          <div class="form-check d-flex justify-content-center mb-4">
-            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33"  />
-            <label class="form-check-label" for="form2Example33">
-              Remember me
-            </label>
-          </div>
-          
-          <div class = "form-group">
-              <div class = "col-sm-offset-2 col-sm-10">
-                <button type = "submit" class = "btn btn-default">Submit</button>
-              </div>
-          </div>
+      <button class="pull-left btn btn-primary" type="submit">Submit form</button>
           
         </form>
     </div>
