9fc87208b75ec987b2b53da532a0390c038a1452
JulieAnneRose96 <putri.juliana.roslan@gmail.com>
2022-06-27 15:40:40 +0800

    rename

--- src/components/login.js
+++ src/components/login.js
@@ -13,13 +13,13 @@ const UserLogin = () => {
       
               <div className="form-group">
                 <label for="inputEmail3" className="col-xs-2 col-md-4 control-label">Email</label>
-                <div className="col-xs-12 col-sm-6 col-md-6">
+                <div className="col-xs-4 col-sm-6 col-md-6">
                   <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
                 </div>
               </div>
               <div className="form-group">
                 <label for="inputPassword3" className="col-xs-2 col-md-4 control-label">Password</label>
-                <div className="col-xs-12 col-sm-6 col-md-6">
+                <div className="col-xs-4 col-sm-6 col-md-6">
                   <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
                 </div>
               </div>
